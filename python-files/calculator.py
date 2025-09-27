193e41ed148a21d1bc4fb806eeef91073fc94333
Amelia <Amelia.Apaydin@outlook.com>
2025-09-27 12:54:15 +0200

    Remove / Comment out style options.

--- python-files/calculator.py
+++ python-files/calculator.py
@@ -28,9 +28,9 @@ def print_entry():
     entry0.insert(0, result)
 
 #Create style
-style = ttk.Style()
-style.configure("Big.TButton", font=("Helvetica", 18))
-style.configure("Big.TEntry", font=("Helvetica", 48))
+# style = ttk.Style()
+# style.configure("Big.TButton", font=("Helvetica", 18))
+# style.configure("Big.TEntry", font=("Helvetica", 48))
 root = tk.Tk()
 root.title("Calculator")
 root.configure(background="lightblue")
@@ -45,22 +45,22 @@ root.configure(background="lightblue")
 root.geometry("600x800+50+50")
 
 # Create input field
-entry0 = ttk.Entry(root, style="Big.TEntry", justify="right")
+entry0 = ttk.Entry(root, justify="right")
 entry0.grid(row=0, column=0, columnspan=3, sticky="nsew")
 
 # Create Labels
-label1 = ttk.Button(root, text="1", style="Big.TButton", command=entry_insert_1).grid(row=1, column=0, padx=3, pady=6, sticky="nsew")
-label2 = ttk.Button(root, text="2", style="Big.TButton", command=entry_insert_2).grid(row=1, column=1, padx=3, pady=6, sticky="nsew")
-label3 = ttk.Button(root, text="3", style="Big.TButton", command=entry_insert_3).grid(row=1, column=2, padx=3, pady=6, sticky="nsew")
-label4 = ttk.Button(root, text="4", style="Big.TButton", command=entry_insert_4).grid(row=2, column=0, padx=3, pady=6, sticky="nsew")
-label5 = ttk.Button(root, text="5", style="Big.TButton", command=entry_insert_5).grid(row=2, column=1, padx=3, pady=6, sticky="nsew")
-label6 = ttk.Button(root, text="6", style="Big.TButton", command=entry_insert_6).grid(row=2, column=2, padx=3, pady=6, sticky="nsew")
-label7 = ttk.Button(root, text="7", style="Big.TButton", command=entry_insert_7).grid(row=3, column=0, padx=3, pady=6, sticky="nsew")
-label8 = ttk.Button(root, text="8", style="Big.TButton", command=entry_insert_8).grid(row=3, column=1, padx=3, pady=6, sticky="nsew")
-label9 = ttk.Button(root, text="9", style="Big.TButton", command=entry_insert_9).grid(row=3, column=2, padx=3, pady=6, sticky="nsew")
-label10 = ttk.Button(root, text="+", style="Big.TButton", command=entry_insert_plus).grid(row=4, column=0, padx=3, pady=6, sticky="nsew")
+label1 = ttk.Button(root, text="1", command=entry_insert_1).grid(row=1, column=0, padx=3, pady=6, sticky="nsew")
+label2 = ttk.Button(root, text="2", command=entry_insert_2).grid(row=1, column=1, padx=3, pady=6, sticky="nsew")
+label3 = ttk.Button(root, text="3", command=entry_insert_3).grid(row=1, column=2, padx=3, pady=6, sticky="nsew")
+label4 = ttk.Button(root, text="4", command=entry_insert_4).grid(row=2, column=0, padx=3, pady=6, sticky="nsew")
+label5 = ttk.Button(root, text="5", command=entry_insert_5).grid(row=2, column=1, padx=3, pady=6, sticky="nsew")
+label6 = ttk.Button(root, text="6", command=entry_insert_6).grid(row=2, column=2, padx=3, pady=6, sticky="nsew")
+label7 = ttk.Button(root, text="7", command=entry_insert_7).grid(row=3, column=0, padx=3, pady=6, sticky="nsew")
+label8 = ttk.Button(root, text="8", command=entry_insert_8).grid(row=3, column=1, padx=3, pady=6, sticky="nsew")
+label9 = ttk.Button(root, text="9", command=entry_insert_9).grid(row=3, column=2, padx=3, pady=6, sticky="nsew")
+label10 = ttk.Button(root, text="+", command=entry_insert_plus).grid(row=4, column=0, padx=3, pady=6, sticky="nsew")
 label11 = ttk.Button(root, text="-", style="Big.TButton").grid(row=4, column=1, padx=3, pady=6, sticky="nsew")
-label12 = ttk.Button(root, text="=", style="Big.TButton", command=print_entry).grid(row=4, column=2, padx=3, pady=6, sticky="nsew")
+label12 = ttk.Button(root, text="=", command=print_entry).grid(row=4, column=2, padx=3, pady=6, sticky="nsew")
 # put widget to the left side and
 # with the label at its bottom
 # label3 = ttk.Button(root, text="Hello", width=20).grid(row=0, column=3)
